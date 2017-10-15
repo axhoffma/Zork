cdf3f5c4ad9354ef551150ccfe9f181308ca6ce9
Austin Hoffmann <axhoffma@purdue.edu>
2017-10-15 14:56:48 -0400

    Add support for adding and removing items, including put and take commands

--- Room.h
+++ Room.h
@@ -9,6 +9,7 @@
 #define ROOM_H_
 
 #include "Object.h"
+#include "Container.hpp"
 #include <iostream>
 #include <unordered_map>
 #include "./rapidXML/rapidxml.hpp"
@@ -18,12 +19,18 @@ class Room : public Object {
 	std::string type = "regular";
 	std::array<std::string, 4> border;
   std::vector<std::string> containers;
+  std::vector<std::string> items;
 
 public:
 	Room(rapidxml::xml_node<>* node);
 	bool exit_check();
-  Room movement(std::string direction, const std::unordered_map<std::string, Room>& roomMap);
-  bool find_container(std::string container);
+  Room movement(std::string direction, const std::unordered_map<std::string, Room>&);
+  bool find_container(std::string);
+  bool find_item(std::string, const std::unordered_map<std::string, Container>&);
+  void remove_item(std::string, std::unordered_map<std::string, Container>&);
+  inline void add_item(std::string item) {
+    items.push_back(item);
+  }
 
 };
 
